b4a2680377f7bee9f5a3542bc07f4b86990fcc92
Fakhri Feki <fakhri_fki@hotmail.com>
2023-02-25 01:33:50 -0800

    Increase max vertical size of Render DgSurf from 2048 to 4096

--- DGUTILS.asm
+++ DGUTILS.asm
@@ -40,7 +40,7 @@
 ; constants
 
 Prec                EQU 12
-MaxResV             EQU 2048
+MaxResV             EQU 4096
 BlendMask           EQU 0x1f
 CMaskB_RGB16        EQU 0x1f   ; blue bits 0->4
 CMaskG_RGB16        EQU 0x3f<<5  ; green bits 5->10
